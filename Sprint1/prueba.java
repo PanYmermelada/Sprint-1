ffeec8d24f83429ca8eeed99ff391e454256ea3b
Diego <azmaeldcv@gmail.com>
2018-09-30 15:11:27 -0400

    pruba1

---
+++ Sprint1/prueba.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Diego
+ */
+public class prueba {
+    public static void main (String args []){
+    System.out.println("holaaaa");
+}
+}
